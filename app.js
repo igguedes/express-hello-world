1fd775371262097f59d5aeb3348ae530eaea2d54
Igor Guedes <igguedes.sousa@gmail.com>
2024-05-26 12:38:41 -0300

    teste asasasa

--- app.js
+++ app.js
@@ -1,279 +1,145 @@
+/**
+ * TODO(developer):
+ * Add your service key to the current folder.
+ * Uncomment and fill in these variables.
+ */
+const projectId = 'meu-projeto-394802';
+const locationId = 'global';
+const agentId = '261c8f13-9a7f-4fcb-b83b-ab599dd8c272';
+const languageCode = 'en'
+const TELEGRAM_TOKEN='7484081454:AAFd7X9ThG2FQGB6rNAA1MCQI9H5xt1jCYs';
+const SERVER_URL='https://teste-igor-express.onrender.com';
+
+const structProtoToJson =
+    require('../../botlib/proto_to_json.js').structProtoToJson;
+
+const express = require('express');
 const axios = require('axios');
-var express = require('express');
-var app = express();
+const bodyParser = require('body-parser');
 
-const url = 'https://novorevan.brisanet.net.br/remoto';
+const API_URL = `https://api.telegram.org/bot${TELEGRAM_TOKEN}`;
+const URI = `/webhook/${TELEGRAM_TOKEN}`;
+const WEBHOOK = SERVER_URL + URI;
 
-var config = {
-    headers : {
-        'Accept': '*/*',
-        'Accept-Language': 'en-US,en;q=0.9',
-        'Connection': 'keep-alive',
-        'Cookie': 'NovoRevan=eyJhdXRoIjp7ImFkbWluaXN0cmFkb3IiOjEsImNoYXQiOltdLCJjaGF0X2hhYmlsaXRhZG8iOjAsImNoYXRfaWQiOi0xLCJjaWRhZGVfaWQiOjEsImNvZGlnb191c3VhcmlvIjoiMDIwNzYxYzAtODU5OC00MGNiLWJjNTUtZTY0MGZmNWRmZmViIiwiZGVwYXJ0YW1lbnRvX2dlcmVudGUiOjAsImRlcGFydGFtZW50b19pZCI6MTgsImRlcGFydGFtZW50b19ub21lIjoiRGVzZW52b2x2aW1lbnRvIiwiZW1haWwiOm51bGwsImVxdWlwZV9pZCI6bnVsbCwiZXJyb3JfY2hhdCI6IiIsImZ1bmNpb25hcmlvX2lkIjoxNCwiaWQiOjIwLCJpbWFnZW0iOiIyMC5wbmciLCJsb2dpbiI6IjA2MS45MTMuMTgzLTc1Iiwibm9tZSI6Iklnb3IgQ2Fyb2xpbm8gR3VlZGVzIGRlIFNvdXNhIiwib3RwX2NvbmZpZ3VyYWRvIjoxLCJvdHBfaGFiaWxpdGFkbyI6MSwicGFndWVicmlzYSI6eyJkb21pbmlvX2lkIjoxfSwicHJlZmVyZW5jaWEiOiJ7XCJ0ZW1hXCI6IFwie1xcXCJ0ZW1hXFxcIjogXFxcImJsdWVcXFwiLCBcXFwidXJsX2NhcGFcXFwiOiBcXFwiXFxcIiwgXFxcInRpcG9fbWFwYVxcXCI6IFxcXCJoeWJyaWRcXFwiLCBcXFwiZmlyc3RfbG9naW5fcmF1bWlsXFxcIjogMH1cIiwgXCJ1cmxfY2FwYVwiOiBcImh0dHBzOlwvXC93YWxscGFwZXJjYXZlLmNvbVwvd3BcL3dwMjM1NTc3OS5qcGdcIiwgXCJ0aXBvX21hcGFcIjogXCJoeWJyaWRcIiwgXCJmaXJzdF9sb2dpbl9yYXVtaWxcIjogMH0iLCJyYXVtaWwiOnsicHJlZmVyZW5jaWEiOnsiZmlyc3RfbG9naW5fcmF1bWlsIjowLCJ0ZW1hIjoie1widGVtYVwiOiBcImJsdWVcIiwgXCJ1cmxfY2FwYVwiOiBcIlwiLCBcInRpcG9fbWFwYVwiOiBcImh5YnJpZFwiLCBcImZpcnN0X2xvZ2luX3JhdW1pbFwiOiAwfSIsInRpcG9fbWFwYSI6Imh5YnJpZCIsInVybF9jYXBhIjoiaHR0cHM6XC9cL3dhbGxwYXBlcmNhdmUuY29tXC93cFwvd3AyMzU1Nzc5LmpwZyJ9fSwicmV2YW4iOnsiYnlwYXNzIjoxfSwic2V0b3JfaWQiOm51bGwsInNldG9yX25vbWUiOm51bGwsInNldG9yZXMiOlt7InNldG9yX2lkIjo4LCJzZXRvcl9ub21lIjoiU2lzdGVtYSAtIERldnMiLCJzdXBlcnZpc29yIjpmYWxzZX1dLCJzZXRvcmVzX2lkcyI6WzhdLCJzaXN0ZW1hX2VzdG9xdWUiOjEsInRlbGVmb25lIjoiODg5OTI3NjQwODIiLCJ0b2tlbiI6IjYyNTZjMGMzNzA2NGY4NTk2YzhmYWIzMmFiNDg4NDM1MzQ5ZjNhYzc1OTgyOTZlYmVhZmIxYzI3MTc4MmExMTgiLCJ0b2tlbl9kYXRhX2NyaWFjYW8iOiIyNVwvMTJcLzIwMjMgMjE6NTc6NDQiLCJ2ZW5kYV9jYW5hbF9pZCI6MTB9LCJleHBpcmVzIjoxNzAzNjM4Njg5fQ----df74be27f0e28e8fbfadad3cd24267a94beb65ef',
-        'Referer': 'https://novorevanh.brisanet.net.br/',
-        'Sec-Fetch-Dest': 'empty',
-        'Sec-Fetch-Mode': 'cors',
-        'Sec-Fetch-Site': 'same-origin',
-        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
-        'X-Auth-Brisanet': '48487864-4bdc-41a4-93d2-328f5874374e',
-        'sec-ch-ua': '"Not_A Brand";v="8", "Chromium";v="120", "Google Chrome";v="120"',
-        'sec-ch-ua-mobile': '?0',
-        'sec-ch-ua-platform': '"macOS"'
-      }
+const app = express();
+app.use(bodyParser.json());
+
+// Imports the Google Cloud Some API library
+const {SessionsClient} = require('@google-cloud/dialogflow-cx');
+/**
+ * Example for regional endpoint:
+ *   const locationId = 'us-central1'
+ *   const client = new SessionsClient({apiEndpoint:
+ * 'us-central1-dialogflow.googleapis.com'})
+ */
+const client = new SessionsClient(
+    {apiEndpoint: locationId + '-dialogflow.googleapis.com'});
+
+// Converts Telgram request to a detectIntent request.
+function telegramToDetectIntent(telegramRequest, sessionPath) {
+  const request = {
+    session: sessionPath,
+    queryInput: {
+      text: {
+        text: telegramRequest.message.text,
+      },
+      languageCode,
+    }
+  };
+
+  return request;
+}
+
+// Converts detectIntent responses to Telegram message requests.
+async function convertToTelegramMessage(responses, chatId) {
+  let replies = [];
+
+  for (let response of responses.queryResult.responseMessages) {
+    let reply;
+
+    switch (true) {
+      case response.hasOwnProperty('text'): {
+        reply = {chat_id: chatId, text: response.text.text.join()};
+        break;
+      };
+
+      /**
+       * The layout for the custom payload responses can be found in these
+       * sites: Buttons: https://core.telegram.org/bots/api#inlinekeyboardmarkup
+       * Photos: https://core.telegram.org/bots/api#sendphoto
+       * Voice Audios: https://core.telegram.org/bots/api#sendvoice
+       */
+      case response.hasOwnProperty('payload'): {
+        reply = await structProtoToJson(response.payload);
+        reply['chat_id'] = chatId;
+        break;
+      };
+
+      default:
+    };
+    if (reply) {
+      replies.push(reply);
+    };
+  }
+
+  return replies;
+}
+
+/**
+ * Takes as input a request from Telegram and converts the request to
+ * detectIntent request which is used to call the detectIntent() function
+ * and finally output the response given by detectIntent().
+ */
+async function detectIntentResponse(telegramRequest) {
+  const sessionId = telegramRequest.message.chat.id;
+  const sessionPath = client.projectLocationAgentSessionPath(
+      projectId, locationId, agentId, sessionId);
+  console.info(sessionPath);
+
+  request = telegramToDetectIntent(telegramRequest, sessionPath);
+  const [response] = await client.detectIntent(request);
+
+  return response;
 };
 
-function formatarCPF(cpf) {
+const setup = async () => {
+  const res = await axios.post(`${API_URL}/setWebhook`, {url: WEBHOOK});
+  console.log(res.data);
+};
 
-    cpf = cpf.replace(/\D/g, '');
-  
-    if (cpf.length !== 11) {
-      return 'CPF inválido';
-    }
+app.post(URI, async (req, res) => {
+  const chatId = req.body.message.chat.id;
+  const response = await detectIntentResponse(req.body);
+  const requests = await convertToTelegramMessage(response, chatId);
 
-    return cpf.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, '$1.$2.$3-$4');
-}
-  
-  
-
-async function localizarCliente(cpf){
-    const path = url + '/venda/clienteV2';
-    const params = {
-        limite: 1,
-        descolcamento: 0,
-        filtro: JSON.stringify({documento: formatarCPF(cpf)})
-    }
-    
-    const response = await axios.get(path,{...config, params});
-    if(response.data.total > 0){
-        return {status: 200, data: response.data.data[0]};
-    }
-    return {status: 404, data: 'Cliente não encontrato'};
-}
-
-//TODO - Simplificar o retorno da rota
-async function lerTimeLine(cliente_id) {
-    const path = url + '/brisasearch/timeline/buscar';
-    const params = {
-        limite: 200,
-        descolcamento: 0,
-        identificador: 'cliente_' + cliente_id
-    }
-    const response = await axios.get(path,{...config, params});
-    if(response.data){
-        return {status: 200, data: response.data};
-    }
-    return {status: 404, data: 'Informações não encontradas'};
-}
-
-function normalizarDadosContrato(item, array){
-    
-        array.push({
-            categoria: item.categoria_nome,
-            endereco:  item.endereco_contrato.logradouro + ', ' + item.endereco_contrato.numero + ', ' + item.endereco_contrato.nome + ', ' + item.endereco_contrato.bairro + ', ' + item.endereco_contrato.cep + ', ' + (item.endereco_contrato.ponto_referencia_1 || '') + ', ' + (item.endereco_contrato.ponto_referencia2 || ''),
-            plano_principal: item.planos.filter(function(plano){
-                if(plano.principal && !plano.cancelado) return plano;
-            }).map(item => item.nome),
-            planos_adicionais: item.planos.filter(function(plano){
-                if(!plano.principal && !plano.cancelado) return plano;
-            }).map(item => item.nome),
-            rota: item.rota_configuracao.rota,
-            olt_id: item.rota_configuracao.olt_id,
-            pon: item.rota_configuracao.pon,
-            olt: item.rota_configuracao.olt_id,
-            splitter_nome: item.rota_configuracao.splitter_nome,
-            splitter_id: item.rota_configuracao.splitter_id,
-            splitter_porta: item.rota_configuracao.splitter_porta
-        });
-
-    
-}
-
-async function lerInformacoesGeraisContratos(cliente_id) {
-
-    const path = url + '/venda/cliente/' + cliente_id + '/contrato';
-    const params = {
-        filtro: JSON.stringify({cancelado: false})
-    }
-
-    
-    const response = await axios.get(path, {...config, params});
-
-    if(response.data){
-        const contratos_inativos = [];
-        const contratos_ativos = [];
-        response.data.data.forEach(function(item){
-            if(item.ativo){
-                normalizarDadosContrato(item, contratos_ativos);
-            } else {
-                normalizarDadosContrato(item, contratos_inativos);
-            }
-        });
-        return {
-            status: 200,
-            data: {
-                quantidade_contratos_ativos: contratos_ativos.length,
-                quantidade_contratos_inativos: contratos_inativos.length,
-                contratos_ativos,
-                contratos_inativos
-            }   
-        }
-    }
-    return {status: 404, data: 'Informações não encontradas'};
-
-}
-
-function rotaComProblema(rota){
-    const rotas_com_problema = [];
-    return rotas_com_problema.some(function(rota){});
-}
-
-async function buscarAnosComFatura(cliente_id) {
-    const path = url + '/financeiro/cliente/' +cliente_id +'/fatura/sumario/anual';
-    
-    try {
-        const response = await axios.get(path, config);
-        if(response.data){
-            return {status: 200, data: {
-                anos_com_faturas: response.data.map(function(item){ return item.ano}),
-                anos_com_fatura_em_aberto : response.data.filter(function(item){ if(item.status == 0) return item}).map(item => item.ano)
-            }};
-        }
-        
-    } catch(e) {
-        console.log(e.message);
-        return {status: 404, data: 'Informações não encontradas'};
-    }
-}
-
-async function buscarMesesComFatura(cliente_id, ano){
-    const path = url + '/financeiro/cliente/' + cliente_id + '/fatura/sumario';
-    const params = {
-        ano
-    }
-
-    const response = await axios.get(path, {...config, params});
-
-    if(response.data){
-        return {status: 200, data: {
-            meses_com_faturas: response.data.map(function(item){return item.data_vencimento}),
-            meses_com_faturas_em_aberto: response.data.filter(item => item.sumario.estado == 0).map(item => item.data_vencimento)
-        }}
+  for (request of requests) {
+    if (request.hasOwnProperty('photo')) {
+      await axios.post(`${API_URL}/sendPhoto`, request).catch(function(error) {
+        console.log(error)
+      })
+    } else if (request.hasOwnProperty('voice')) {
+      await axios.post(`${API_URL}/sendVoice`, request).catch(function(error) {
+        console.log(error)
+      })
     } else {
-        return {status: 404, data: 'Informações não encontradas'};
+      await axios.post(`${API_URL}/sendMessage`, request).catch(function(error) {
+        console.log(error)
+      })
     }
-}
+  }
 
-//TODO associar faturas a endereços
-async function buscarFaturasPorCompetencia(cliente_id, meses_com_fatura) {
-    const path = url + '/financeiro/cliente/' + cliente_id + '/faturas';
-
-    const ano = meses_com_fatura[0].split('-')[0];
-
-    const faturas = [];
-
-    for(var i=0; i<meses_com_fatura.length; i++) {
-        const params = {
-            data_vencimento: meses_com_fatura[i],
-            fatura_em_aberto: 0
-        }
-        const response = await axios.get(path, {...config, params});
-        var obj = {};
-        obj[meses_com_fatura[i]] = response.data.detalhamento.map((function(item){
-            return {
-                id: item.id,
-                descricao: item.descricao,
-                vencimento: item.data_vencimento,
-                estado: item.estado,
-                valor: item.valor,
-                lancamentos: item.lancamentos.map(function(item){
-                    return {
-                        nome: item.nome,
-                        tipo: item.tipo_lancamento == "d"? "Débito" : "Crédito",
-                        valor: item.valor
-                    }
-                })
-            }
-        }));
-
-        faturas.push(obj);
-    }
-
-    if(faturas.length){
-        const objRetorno = {};
-        objRetorno[ano] = faturas;
-        return {status: 200, data: objRetorno};
-    } else {
-        return {status: 400, data: 'Informações não encontradas'}
-    }
-}
-
-async function lerFinanceiro(cliente_id) {
-    const anos_com_faturas = (await buscarAnosComFatura(cliente_id)).data;
-
-    var meses_com_faturas = [];
-    var meses_com_faturas_em_aberto = [];
-
-    
-
-    for(ano in anos_com_faturas.anos_com_faturas){
-        const response = await buscarMesesComFatura(cliente_id, anos_com_faturas.anos_com_faturas[ano]);
-        response.data.meses_com_faturas.forEach(item => meses_com_faturas.push(item));
-        response.data.meses_com_faturas_em_aberto.forEach(item => meses_com_faturas_em_aberto.push(item));
-    }
-
-    const faturas = await buscarFaturasPorCompetencia(cliente_id, meses_com_faturas);
-
-    return {
-        anos_com_faturas: anos_com_faturas.anos_com_faturas,
-        anos_com_fatura_em_aberto: anos_com_faturas.anos_com_fatura_em_aberto,
-        meses_com_faturas,
-        meses_com_faturas_em_aberto,
-        faturas: faturas.data
-    }
-}
-
-async function lerChamados(cliente_id) {
-
-}
-
-
-app.get('/localizar_cliente/:cpf', async function(req, res){
-    var response = await localizarCliente(req.params.cpf);
-    res.json(response);
+  return res.send();
 });
 
-app.get('/informacoes_gerais_contrato/:cliente_id', async function(req, res){
-    var response = await lerInformacoesGeraisContratos(req.params.cliente_id);
-    res.json(response);
+const listener = app.listen(process.env.PORT, async () => {
+  console.log(
+      'Your Dialogflow integration server is listening on port ' +
+      listener.address().port);
+
+  await setup();
 });
 
-app.get('/informacoes_financeiras/:cliente_id', async function(req, res){
-    var response = await lerFinanceiro(req.params.cliente_id);
-    res.json(response)
-});
-
-async function main() {
-    const a = await buscarFaturasPorCompetencia(107, [
-        '2023-01', '2023-02',
-        '2023-03', '2023-04',
-        '2023-05', '2023-06',
-        '2023-07', '2023-08',
-        '2023-09', '2023-10',
-        '2023-11', '2023-12'
-      ]);
-    console.log(a);
-    a.data['2023'].forEach(function(item){
-        console.log(item)
-        if(item.lancamentos){
-            item.lancamentos.forEach(a => console.log(a));
-        }
-    }); 
-
-}
-
-// main();
-
-app.listen(3000, function(){
-    console.log('Servidor rodando na porta 3000');
-});
-
+module.exports = {
+  telegramToDetectIntent,
+  convertToTelegramMessage
+};
